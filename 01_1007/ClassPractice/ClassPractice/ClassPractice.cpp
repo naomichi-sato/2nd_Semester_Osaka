77fce78b2f67fae9569c0e2e090abf2529375d3f
NOTE-SATOU\satou <naomichi.sato.1981@gmail.com>
2020-10-07 14:24:06 +0900

    リファクタリング作業その１ 　・クラス名の変更 　・クラスの宣言と定義部分をファイルに分割 　・共通で使うヘッダーファイルをCommon.hに移動

--- 01_1007/ClassPractice/ClassPractice/ClassPractice.cpp
+++ 01_1007/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -1,52 +1,20 @@
 
-#include <stdlib.h>
-#include <stdio.h>
-
-class Fighter
-{
-public:
-	Fighter();
-	~Fighter();
-
-	int m_hp;
-	int m_str;
-	int m_mag;
-	int m_tec;
-	int m_spd;
-	int m_def;
-	int m_luc;
-	int m_mde;
-};
-
-Fighter::Fighter()
-{
-	m_hp	= 0;
-	m_str	= 0;
-	m_mag	= 0;
-	m_tec	= 0;
-	m_spd	= 0;
-	m_def	= 0;
-	m_luc	= 0;
-	m_mde	= 0;
-}
-
-Fighter::~Fighter()
-{
-}
+#include "Common.h"
+#include "Unit.h"
 
 int main()
 {
-	Fighter fighter;
+	Unit unit;
 
-	// fighterないの各変数をprintfで出力
-	printf("m_hp  = %d\n", fighter.m_hp);
-	printf("m_str = %d\n", fighter.m_str); 
-	printf("m_mag = %d\n", fighter.m_mag); 
-	printf("m_tec = %d\n", fighter.m_tec); 
-	printf("m_spd = %d\n", fighter.m_spd); 
-	printf("m_def = %d\n", fighter.m_def); 
-	printf("m_luc = %d\n", fighter.m_luc); 
-	printf("m_mde = %d\n", fighter.m_mde); 
+	// Unitないの各変数をprintfで出力
+	printf("m_hp  = %d\n", unit.m_hp);
+	printf("m_str = %d\n", unit.m_str); 
+	printf("m_mag = %d\n", unit.m_mag); 
+	printf("m_tec = %d\n", unit.m_tec); 
+	printf("m_spd = %d\n", unit.m_spd); 
+	printf("m_def = %d\n", unit.m_def); 
+	printf("m_luc = %d\n", unit.m_luc); 
+	printf("m_mde = %d\n", unit.m_mde); 
 
 	system("pause");
 	return 0;
