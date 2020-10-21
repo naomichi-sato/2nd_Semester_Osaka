06755c40e809a05c9e4ef88976d7d6f9b5933e37
NOTE-SATOU\satou <naomichi.sato.1981@gmail.com>
2020-10-21 17:17:14 +0900

    純粋仮想関数の実践
    デストラクタには、virtualをつけておく
    基底クラスのポインタに派生クラスを渡して、処理を行うという考え方をポリモーフィズムと言ったりする。

--- 03_1021/Practice03/Practice03_02/Base.h
+++ 03_1021/Practice03/Practice03_02/Base.h
@@ -9,6 +9,11 @@ public:
 	Base();
 	virtual ~Base();
 
+public:
+	// 継承先で、それぞれ処理の違うことがわかっている関数を
+	// 純粋仮想関数として基底クラスに宣言
+	virtual void Exec() = 0;
+	virtual void Draw() = 0;
 
 public:
 	// 引数の矩形情報と、自分が当たっているか判定
--- 03_1021/Practice03/Practice03_02/Player.cpp
+++ 03_1021/Practice03/Practice03_02/Player.cpp
@@ -1,12 +1,15 @@
 ﻿
 #include "Player.h"
+#include <stdio.h>
 
 Player::Player()
 {
+	printf("Player : コンストラクタ\n");
 }
 
 Player::~Player()
 {
+	printf("Player : デストラクタ\n");
 }
 
 void Player::Exec()
