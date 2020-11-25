af2da8c849795537c7afc64fd315b2940f63ab80
NOTE-SATOU\satou <naomichi.sato.1981@gmail.com>
2020-11-25 15:06:05 +0900

    【第７回】実習その２
    比較演算子( ==, !=, >, <, >=, <=)のオーバーロード実践

---
+++ 07_1125/Practice07/Practice07_02_Operator_Compare/Practice07_02_Operator_Compare.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+#include "Employee.h"
+
+int main()
+{
+	Employee satou( Employee::POST::STAFF );
+	Employee deguchi( Employee::POST::PRESIDENT );
+
+	satou.ShowPost();
+	deguchi.ShowPost();
+
+	if( satou > deguchi )
+	{
+		printf("satouさんの方が偉い\n");
+	}
+	else
+	{
+		printf("deguchiさんの方が偉い……かも？\n");
+	}
+
+	return 0;
+}
+
